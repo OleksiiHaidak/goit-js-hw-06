02da5eb687b85f302cb7fd869e56c2debecf474f
11Alex111 <xalexey.g@gmail.com>
2023-08-13 19:55:32 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputMenu = document.querySelector('#controls input[type="number"]');
+const boxElements = document.querySelector('#boxes');
+const createBtn = document.querySelector('#controls button[data-create]');
+const destroyBtn = document.querySelector('#controls button[data-destroy]');
+
+
+createBtn.addEventListener('click', () => {
+  const amount = Number(inputMenu.value);
+  createBoxes(amount);
+});
+
+function createBoxes(amount) { 
+  let boxItems = "";
+  let dimension = 30;
+  for (let i = 1; i <= amount; i += 1) { 
+    boxItems += `<div style="background-color: ${getRandomHexColor()}; width: ${dimension}px; height:${dimension}px; margin-top: 10px;"></div>`;
+    dimension += 10;
+  }
+  boxElements.insertAdjacentHTML("beforeend", boxItems);
+};
+
+
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() { 
+  boxElements.innerHTML = '';
+  inputMenu.value = '';
+};
